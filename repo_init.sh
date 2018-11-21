ea3009e30d51204c1ce47d14b9291e02ba5a14ee
sliu <sz_liusha@spectratech.com>
2018-11-21 08:44:59 +0800

    Add bash label and comments for repo init -u

--- repo_init.sh
+++ repo_init.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
 mkdir my_repo
 cd my_repo
+#repo init's -u needs not to be a bare git repository
 repo --trace init -u ../ -m default.xml -b master --repo-url='https://mirrors.tuna.tsinghua.edu.cn/git/git-repo'
